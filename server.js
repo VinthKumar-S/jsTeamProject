e4093ebc4600e584be54507bc313bd04caf6563c
SIVAVETRIVEL <sivavetrivel07502@gmail.com>
2024-07-11 15:29:51 +0530

    add

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Define a route for the homepage
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'testnews.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
